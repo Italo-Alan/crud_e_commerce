a236f90dc2429af661fdfc219eda1e19a464b250
Italo-Alan <italoalan123@gmail.com>
2023-05-21 23:42:21 -0300

    feat: Consertando o ambiente Angular

--- crud_e_commerce_angular/src/app/app.component.spec.ts
+++ crud_e_commerce_angular/src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule],
     declarations: [AppComponent]
   }));
 
@@ -12,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'crud_e_commerce'`, () => {
+  it(`should have as title 'crud_e_commerce_angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('crud_e_commerce');
+    expect(app.title).toEqual('crud_e_commerce_angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('crud_e_commerce app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('crud_e_commerce_angular app is running!');
   });
 });
--- crud_e_commerce_angular/src/app/app.component.ts
+++ crud_e_commerce_angular/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'crud_e_commerce';
+  title = 'crud_e_commerce_angular';
 }
